d7a0245c1b976cdd1f6c3dbc874fee13ccc4b905
hashaneranda <hashaneranda@gmail.com>
2022-07-16 20:37:57 +0530

    ios config added

--- ios/VisionCameraImageLabeler.m
+++ ios/VisionCameraImageLabeler.m
@@ -1,33 +1,55 @@
-
-
 #import <VisionCamera/FrameProcessorPlugin.h>
 #import <VisionCamera/Frame.h>
 #import <MLKit.h>
 
 // Example for an Objective-C Frame Processor plugin
-@interface QRCodeFrameProcessorPluginObjC : NSObject
+
+@interface ImageLabelerPlugin : NSObject
 
 + (MLKImageLabeler*) labeler;
 
+
 @end
 
-@implementation QRCodeFrameProcessorPluginObjC
+@implementation ImageLabelerPlugin
+
 
 + (MLKImageLabeler*) labeler {
   static MLKImageLabeler* labeler = nil;
+
   if (labeler == nil) {
-    MLKImageLabelerOptions* options = [[MLKImageLabelerOptions alloc] init];
+  NSString *path = [[NSBundle mainBundle] pathForResource:@"model" ofType:@"tflite"];
+    NSLog(@"PATH:", path);
+    MLKLocalModel *localModel =
+      [[MLKLocalModel alloc] initWithPath:path];
+
+  MLKCustomImageLabelerOptions *options =
+      [[MLKCustomImageLabelerOptions alloc] initWithLocalModel:localModel];
+   NSLog(@"OPTIONS", options);
+  options.confidenceThreshold = @(0.0);
+  labeler =
+      [MLKImageLabeler imageLabelerWithOptions:options];
+
     labeler = [MLKImageLabeler imageLabelerWithOptions:options];
+
   }
   return labeler;
 }
 
 static inline id labelImage(Frame* frame, NSArray* arguments) {
+
   MLKVisionImage *image = [[MLKVisionImage alloc] initWithBuffer:frame.buffer];
+  NSLog(@"VISION CAMERA IMAGE LABELER: Testing this out:, %@", image);
   image.orientation = frame.orientation; // <-- TODO: is mirrored?
 
-  NSError* error;
-  NSArray<MLKImageLabel*>* labels = [[QRCodeFrameProcessorPluginObjC labeler] resultsInImage:image error:&error];
+  NSError* error = nil;
+ NSArray<MLKImageLabel*>* labels = [[ImageLabelerPlugin labeler] resultsInImage:image error:&error];
+
+  if(error){
+    NSLog(@"Error finding Labels: %@", error);
+  } else {
+    NSLog(@"No Error: %@", error);
+  }
 
   NSMutableArray* results = [NSMutableArray arrayWithCapacity:labels.count];
   for (MLKImageLabel* label in labels) {
